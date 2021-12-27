c6a804de0209a18b333a445bf92b4bd3c58101dd
Francesco Fenomeno <francesco.fenomeno99@gmail.com>
2021-12-27 23:00:24 +0100

    update

---
+++ WatIDo.py
@@ -0,0 +1,22 @@
+from time import localtime
+
+activities = {8: 'Sleeping',
+              13: 'School',
+              14: 'Launch',
+              17: 'Working',
+              18: 'Commuting',
+              20: 'Eating',
+              21: 'Resting',
+              24: 'I am Hacking bitches!',
+              6: 'I am Hacking bitches!' }
+
+time_now = localtime()
+hour = time_now.tm_hour
+
+for activity_time in sorted(activities.keys()):
+    if hour < activity_time:
+        print (activities[activity_time])
+        break
+else:
+    print ('Unknown, AFK or sleeping!')
+input()
